3c263c441cbf6d10020fed81b2d7081fcf65c33a
alauper <adam.lauper@acimacredit.com>
2023-01-06 14:47:44 -0700

    adding from_hash

---
+++ lib/denv/sources/hash.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class DEnv
+  class Sources
+    class Hash < Base
+      def initialize(hash)
+        @hash = hash
+        super()
+      end
+
+      def type
+        'hash'
+      end
+
+      def key
+        "#{type[0, 1]}:#{@hash.object_id}"
+      end
+
+      private
+
+      def set_entries
+        @hash.each do |key, value|
+          add(key, value)
+        end
+      end
+    end
+  end
+
+  def self.from_hash(hash)
+    add(Sources::Hash.new(hash))
+  end
+
+  def self.from_hash!(hash)
+    from_hash(hash)
+    env!
+  end
+end
